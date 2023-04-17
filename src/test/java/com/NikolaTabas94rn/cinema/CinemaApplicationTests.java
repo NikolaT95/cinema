9c15806be71c6f1ff67cd824d9d75d92bcdaa526
Nikola Tabas <nikolatabas1@gmail.com>
2023-04-17 18:33:45 +0200

    test prazan projekat

---
+++ src/test/java/com/NikolaTabas94rn/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NikolaTabas94rn.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
